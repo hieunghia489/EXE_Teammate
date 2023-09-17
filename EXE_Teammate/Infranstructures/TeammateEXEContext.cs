4f5338ea6e01bd2ff9d0fea46f56b57db666b59a
Phan Dang Hong Duc <ducpdhse160435@fpt.edu.vn>
2023-09-17 13:14:19 +0700

    fix appsettings.json

--- EXE_Teammate/Infranstructures/TeammateEXEContext.cs
+++ EXE_Teammate/Infranstructures/TeammateEXEContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace Domain.Models
 {
@@ -34,10 +35,18 @@ namespace Domain.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=SAVOY\\MSSQLSERVER01;uid=sa;password=123456;Database=TeammateEXE;Trusted_Connection=true;Encrypt=false");
+                optionsBuilder.UseSqlServer(GetConnectionString());
             }
         }
 
+        public string GetConnectionString()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfiguration configuration = builder.Build();
+            return configuration.GetConnectionString("DefaultConnection");
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Course>(entity =>
